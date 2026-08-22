22005405f660bb7a5424eb4161b172ec40edbb27
chetna-code2009 <chetnarawar974@gmail.com>
2026-08-22 15:52:08 +0530

    Fix formatting and update employee program header

--- basics.c
+++ basics.c
@@ -1,11 +1,12 @@
-//👨‍💼EMPLOYEE PROGRAM👨‍💼
+//👩‍💼 EMPLOYEE PROGRAM 👩‍💼
 #include <stdio.h>
 int main()
 {
     int age = 25;
     char grade = 'A';
     float salary = 25000.67;
- printf("%d\n",age);
- printf("%c\n",grade); 
- printf("%f\n",salary);  
+    printf("%d\n",age);
+    printf("%c\n",grade); 
+    printf("%2f\n",salary); 
+    return 0; 
 }
