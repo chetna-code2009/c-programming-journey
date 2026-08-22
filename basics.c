8c786c01f7bd304d17a0f68a622365aa94802ec4
chetna-code2009 <chetnarawar974@gmail.com>
2026-08-22 15:47:55 +0530

    Add employee program to display age, grade, and salary

---
+++ basics.c
@@ -0,0 +1,11 @@
+//👨‍💼EMPLOYEE PROGRAM👨‍💼
+#include <stdio.h>
+int main()
+{
+    int age = 25;
+    char grade = 'A';
+    float salary = 25000.67;
+ printf("%d\n",age);
+ printf("%c\n",grade); 
+ printf("%f\n",salary);  
+}
