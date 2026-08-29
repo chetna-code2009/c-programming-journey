3cb86dacd9d4b2b9b8e05c7d55ee564291874175
chetna-code2009 <chetnarawar974@gmail.com>
2026-08-29 13:27:06 +0530

    Add newline at the end of basics.c

--- basics.c
+++ basics.c
@@ -5,8 +5,10 @@ int main()
     int age = 25;
     char grade = 'A';
     float salary = 25000.67;
+
     printf("%d\n",age);
     printf("%c\n",grade); 
     printf("%2.2f\n",salary); 
+    
     return 0; 
 }
