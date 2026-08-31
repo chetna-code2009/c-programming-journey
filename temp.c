f032d84306712e91fd9732804d10473a8979d8d3
chetna-code2009 <chetnarawar974@gmail.com>
2026-08-31 19:57:33 +0530

    Add temperature converter program

--- temp.c
+++ temp.c
@@ -1,6 +1,6 @@
-#include <conio.h>;
-#include <stdio.h>;
-int main();
+#include <conio.h>
+#include <stdio.h>
+int main()                          
 {
     float celcius,fahrenheit;
     printf("Enter temperature in celcius: ");
@@ -8,4 +8,5 @@ int main();
     fahrenheit = (celcius * 9/5) + 32;
     printf("Temperature in fahrenheit: %f", fahrenheit);
     return 0;
+
 }
